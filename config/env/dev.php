b996a04f4f88311b5c50da8955438503b5f80d52
Oleksandr Mykhailiuta <ercling@gmail.com>
2017-02-22 10:38:34 +0200

    Changed env.php path

--- config/env/dev.php
+++
@@ -1,9 +0,0 @@
-<?php return [
-    'APP_ENV' => 'production',
-    'BASE_DIR' => '/home/wdata/mail/',
-
-    // MYSQL
-    'MYSQL_DATABASE' => 'realmetric',
-    'MYSQL_USERNAME' => 'root',
-    'MYSQL_PASSWORD' => 'root',
-];
