c4d06d2512141d66c0966b470a396a8940798ee8
Andrey Reinwald <andreinwald@gmail.com>
2017-02-22 19:06:10 +0200

    Fixed env path

--- public/index.php
+++ public/index.php
@@ -1,11 +1,7 @@
 <?php
 require __DIR__ . '../../vendor/autoload.php';
 
-if (is_readable(__DIR__ . '/../config/production/env.php')) {
-    $config = require __DIR__ . '/../config/production/env.php';
-} else {
-    $config = require __DIR__ . '/../config/dev/env.php';
-}
+$config = require __DIR__ . '/../config/env.php';
 $dependencies = require __DIR__ . '/../app/dependencies.php';
 $middleware = require __DIR__ . '/../app/middleware.php';
 $routes = require __DIR__ . '/../app/routes.php';
