5b694da07447713a90130d52fb4cc6a6efc45a92
Andrey Reinwald <andreinwald@gmail.com>
2017-02-23 13:01:15 +0200

    Write time in Commands

--- app/Commands/AbstractCommand.php
+++ app/Commands/AbstractCommand.php
@@ -2,7 +2,15 @@
 
 namespace App\Commands;
 
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
 abstract class AbstractCommand extends \Symfony\Component\Console\Command\Command
 {
     use \App\Injectable;
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln(PHP_EOL . 'Start at ' . date('Y-m-d H:i:s'));
+    }
 }
