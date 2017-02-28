2cd8b646d0ccea29db6426765d90621fbadce01f
Oleksandr Mykhailiuta <ercling@gmail.com>
2017-02-28 16:08:13 +0200

    CHanged slices response format

--- app/Controllers/SlicesController.php
+++ app/Controllers/SlicesController.php
@@ -16,18 +16,19 @@ class SlicesController extends AbstractController
         $yesterdayTotals = $this->mysql->dailySlices
             ->setTable(DailySlicesModel::TABLE_PREFIX . date('Y_m_d', strtotime('-1 day')))
             ->getByMetricId($attributes['metric_id']);
-        $slices = array_column($this->mysql->slices->getByIds(array_column($totals, 'slice_id')), 'name', 'id');
-        $yesterdaySlices = array_column($this->mysql->slices->getByIds(array_column($yesterdayTotals, 'slice_id')), 'name', 'id');
+        $slices = array_column(
+            $this->mysql->slices->getByIds(
+                array_merge(
+                    array_column($totals, 'slice_id'),
+                    array_column($yesterdayTotals, 'slice_id'))
+                ), 'name', 'id');
 
         return $this->jsonResponse([
             'values' => [
                 date('Y-m-d') => $totals,
                 date('Y-m-d', strtotime('-1 day')) => $yesterdayTotals,
             ],
-            'slices' => [
-                date('Y-m-d') => $slices,
-                date('Y-m-d', strtotime('-1 day')) => $yesterdaySlices,
-            ]
+            'slices' => $slices
 
         ]);
     }
