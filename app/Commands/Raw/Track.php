22f891c6d698d8d27b6489bd49173bd58e96bd9e
Andrey Reinwald <andreinwald@gmail.com>
2017-04-24 13:13:38 +0300

    Fix place count

--- app/Commands/Raw/Track.php
+++ app/Commands/Raw/Track.php
@@ -14,7 +14,7 @@ class Track extends AbstractCommand
     {
         $rawEvents = [];
         $i = 0;
-        while (($event = $this->redis->sPop(Keys::REDIS_SET_TRACK_QUEUE)) && $i < 10000) {
+        while (($event = $this->redis->sPop(Keys::REDIS_SET_TRACK_QUEUE)) && $i < 3500) {
             $rawEvents[] = json_decode($event, true);
             $i++;
         }
