14d1a6c011601f662cdd7cf4e211a38baed1019b
Andrey Reinwald <andreinwald@gmail.com>
2017-05-29 17:05:20 +0300

    Update router

--- config/middleware.php
+++ config/middleware.php
@@ -50,6 +50,6 @@
         return $delegate->process($request);
     },
 
-    new FastRouteMiddleware(require __DIR__ . '/routes.php', '\App\Http\Controllers\NotFoundController::showMessage'),
+    new \FastRouteMiddleware\Router(require __DIR__ . '/routes.php', '\App\Http\Controllers\NotFoundController::showMessage'),
     new \App\Http\Middleware\RequestHandlerMiddleware(),
 ];
