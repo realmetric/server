df5d718283a4386b56010196ca73a095d4425284
Oleksandr Mykhailiuta <mykhailiuta.aleksandr@corp.mailfire.io>
2017-08-14 10:39:21 +0300

    Add AggrDailyMetrics

---
+++ app/Models/DailyMetricTotalsModel.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\Models;
+
+
+class DailyMetricTotalsModel extends AbstractModel
+{
+    const TABLE_PREFIX = 'daily_metric_totals_';
+    const TABLE = self::TABLE_PREFIX . '2017_01_01'; // Just for example
+
+    public function __construct($connection)
+    {
+        parent::__construct($connection);
+
+        $this->setTableFromTimestamp(time());
+    }
+
+    public function setTableFromTimestamp(int $timestamp)
+    {
+        $this->setTable(self::TABLE_PREFIX . date('Y_m_d', $timestamp));
+        $this->createTableIfNotExists();
+        return $this;
+    }
+
+    protected function createTable($name)
+    {
+        if ($this->shema()->hasTable($name)) {
+            return;
+        }
+
+        $this->shema()->create($name, function ($table) {
+            /** @var \Illuminate\Database\Schema\Blueprint $table */
+            $table->increments('id');
+            $table->unsignedSmallInteger('metric_id');
+            $table->integer('value');
+            $table->float('diff')->default(0);
+
+            $table->unique(['metric_id']);
+        });
+    }
+
+    public function dropTable($datePart)
+    {
+        $name = self::TABLE_PREFIX . $datePart;
+        return $this->shema()->dropIfExists($name);
+    }
+
+}
