7fccb0ff701f3f2f24de8a2cdef656f5d58f03fd
Andrey Reinwald <andreinwald@gmail.com>
2020-07-05 19:09:20 +0300

    Created Intersect helper

--- app/Commands/Daily/SliceIntersect.php
+++ app/Commands/Daily/SliceIntersect.php
@@ -4,6 +4,7 @@ namespace App\Commands\Daily;
 
 
 use App\Commands\AbstractCommand;
+use App\Events\Intersect;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -14,21 +15,26 @@ class SliceIntersect extends AbstractCommand
         while (1) {
             $timestart = microtime(true);
             $this->process();
-            echo 'minute done in ' . (microtime(true) - $timestart);
+            echo 'minute done in ' . (microtime(true) - $timestart) . "\n";
         }
     }
 
     private function process()
     {
+        $intersect = new Intersect();
+
         $timestamp = time() - 120;
         $minute = date('H', $timestamp) * 60 + date('i', $timestamp);
 
         // @TODO WRONG INTERSECT between metric and slices - already aggregated
         $metrics = $this->mysql->dailyMetrics->getMetricsByMinute($minute);
         foreach ($metrics as $metricId) {
-            $slices = $this->mysql->dailySlices->getSlicesByMetric($metricId, $minute);
-            $slices = array_slice($slices, 0, 10);
-            $this->mysql->dailySliceIntersect10->createOrIncrement($metricId, $slices, mt_rand(23423, 12312312), $minute);
+            $slicesAll = $this->mysql->dailySlices->getSlicesByMetric($metricId, $minute);
+            $slicesAll = array_slice($slicesAll, 0, 10);
+            $slicesIntersect = $intersect->getIntersect($slicesAll, 10);
+            foreach ($slicesIntersect as $slices) {
+                $this->mysql->dailySliceIntersect10->createOrIncrement($metricId, $slices, mt_rand(23423, 12312312), $minute);
+            }
         }
     }
 }
