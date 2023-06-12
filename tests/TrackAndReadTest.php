cbdaf93b099b853f1fcd3ba06c57524b7417d983
andreinwald <andreinwald@gmail.com>
2023-06-12 21:45:40 +0200

    tests

--- tests/TrackAndReadTest.php
+++ tests/TrackAndReadTest.php
@@ -3,6 +3,10 @@
 namespace App\Tests;
 
 use App\Library\EventSaver;
+use App\Model\MetricsModel;
+use App\Model\MonthlyMetricsModel;
+use App\Model\MonthlySlicesModel;
+use App\Model\SlicesModel;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class TrackAndReadTest extends KernelTestCase
@@ -13,7 +17,34 @@ class TrackAndReadTest extends KernelTestCase
         $container = self::$kernel->getContainer();
         /** @var EventSaver $eventSaver */
         $eventSaver = $container->get(EventSaver::class);
-        $eventSaver->save('test', 1, time(), ['sort' => 'first']);
-        $this->assertEquals(1, 1);
+        $metricName = 'test';
+        $sliceCategory = 'quality';
+        $sliceName = 'best';
+        $value = 3;
+        $time = strtotime('10 days ago');
+        $date = date('Y-m-d', $time);
+        $eventSaver->save('test', $value, $time, [$sliceCategory => $sliceName]);
+
+        /** @var MetricsModel $metricsModel */
+        $metricsModel = $container->get(MetricsModel::class);
+        $metricId = $metricsModel->getId($metricName);
+
+        /** @var SlicesModel $slicesModel */
+        $slicesModel = $container->get(SlicesModel::class);
+        $sliceId = $slicesModel->getId($sliceCategory, $sliceName);
+
+        /** @var MonthlyMetricsModel $monthlyMetricsModel */
+        $monthlyMetricsModel = $container->get(MonthlyMetricsModel::class);
+        $this->assertEquals(
+            [['date' => $date, 'value' => $value]],
+            $monthlyMetricsModel->getByMetricId($metricId)
+        );
+
+        /** @var MonthlySlicesModel $monthlySlicesModel */
+        $monthlySlicesModel = $container->get(MonthlySlicesModel::class);
+        $this->assertEquals([
+            ['date' => $date, 'value' => $value]],
+            $monthlySlicesModel->getValues($metricId, $sliceId)
+        );
     }
 }
